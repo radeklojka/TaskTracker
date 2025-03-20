b44d28d0b96164bee788f2c7f9057d46adb5572b
radeklojka <radek.lojka@gmail.com>
2025-03-20 21:14:09 +0100

    x

--- function.py
+++ function.py
@@ -1,16 +1,19 @@
-def add():
-    ask = str(input("Add a task: ")).strip()
+def add(database):
+    task = str(input("Add a task: ")).strip()
     database.append(task)
     print(database)
 
-def show():
+def show(database):
     print(database)
 
-def delete():
+def delete(database):
     print(database)
     index = int(input("Which task do you wish to delete (type index of the task): "))
-    database.pop(index)
-    print(database)
+    if 0 <= index < len(database):
+        database.pop(index)
+        print("Task at index {index} is deleted")
+    else:
+        print(database)
 
 def exit():
     print("Goodbye!")
